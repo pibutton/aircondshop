52a19635107aa1ee8e0b232987f012a6ee7991f8
Istvan Gombos <gmbs.istvan@gmail.com>
2022-05-26 05:03:20 +0200

    Commit

---
+++ src/main/java/hu/nye/progkor/aircondshop/AircondshopApplication.java
@@ -0,0 +1,14 @@
+package hu.nye.progkor.aircondshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class AircondshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AircondshopApplication.class, args);
+	}
+	
+}
---
+++ src/test/java/hu/nye/progkor/aircondshop/AircondshopApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.nye.progkor.aircondshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AircondshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
